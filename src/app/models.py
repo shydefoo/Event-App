69086b96ab48d7f7346e9cc79c95bb5091bd5ca4
shydefoo <shidefoo@gmail.com>
2019-03-05 13:43:00 +0800

    added category to events

--- src/app/models.py
+++ src/app/models.py
@@ -9,15 +9,19 @@ class UserAccount(models.Model):
     username = models.CharField(max_length=50)
     password = models.CharField(max_length=16)
 
+class Category(models.Model):
+    category = models.CharField(max_length=200)
 
 class Event(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4(), editable=False)
     date = models.DateField(auto_created=True, auto_now_add=True)
     description = models.CharField(max_length=2000)
+    category = models.ManyToManyField(Category)
     title = models.CharField(max_length=200)
     location = models.CharField(max_length=20)
     participants = models.ManyToManyField(UserAccount, related_name='participants', null=True, blank=True)
     likes = models.ManyToManyField(UserAccount, related_name='likes', null=True, blank=True)
+
 
 
 class Comment(models.Model):
--- src/utils/serializers/serializer_response_classes.py
+++ src/utils/serializers/serializer_response_classes.py
@@ -10,13 +10,15 @@ class SingleEvent(BaseSingleEntity):
         self.date = event.date.strftime('%d-%m-%y')
         self.description = event.description
         self.title = event.title
+        self.category = list(map(self.extract_category, list(event.category.all())))
         self.id = self.extract_id(event)
         self.likes = list(map(self.extract_id, list(event.likes.all())))
         self.participants = list(map(self.extract_id, list(event.participants.all())))
 
     # def extract_id(self, object):
     #     return object.id.hex
-
+    def extract_category(self, category):
+        return category.category
 
 class SingleComment(BaseSingleEntity):
     def __init__(self, comment):
