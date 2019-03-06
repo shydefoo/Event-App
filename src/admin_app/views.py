f9ec2ed854a3f75c3621b3349a388d711180d125
shydefoo <shidefoo@gmail.com>
2019-03-06 17:38:39 +0800

    added card block to base template. admin now displays pictures uploaded of event

--- src/admin_app/views.py
+++ src/admin_app/views.py
@@ -106,28 +106,46 @@ def event_view(request, event_id):
         single_event = SingleEventForm(event)
         event_form = EventForm(initial=single_event.__dict__)
         photo_form = PhotoForm()
-        context = {'event_id':event_id,
+
+        event_images = event.photo_set.all()
+
+        context = {'event_id': event_id,
                    'form': event_form,
-                   'photo_form': photo_form}
+                   'photo_form': photo_form,
+                   'images':event_images,
+                   }
+
         return render(request, 'admin_app/event_view.html', context=context)
+
     if request.method == 'POST':
         event = get_object_or_404(Event, pk=event_id)
         f = EventForm(request.POST, instance=event)
-        f.save()
-        return HttpResponseRedirect(reverse('home'))
+        photo_form = PhotoForm(request.POST, request.FILES)
+        if f.is_valid():
+            f.save()
+        if photo_form.is_valid():
+            photo_form.save(commit=False)
+            instance = photo_form.instance
+            instance.event = get_object_or_404(Event, pk=event_id)
+            instance.save()
+            return HttpResponseRedirect(reverse('home'))
+        else:
+            logger.error(photo_form.errors)
+            return HttpResponse('error')
+        # return HttpResponseRedirect(reverse('home'))
 
-# @require_http_methods(['GET', 'POST'])
-# @validate_staff_status(direct_login_page)
-# def create_event_view(request):
-#     if request.method == 'GET':
-#         context = {
-#             'form': EventForm
-#         }
-#         return render(request, 'admin_app/create_event_view.html', context=context)
-#     if request.method == 'POST':
-#         event = EventForm(request.POST)
-#         event.save()
-#         return HttpResponseRedirect(reverse('home'))
+@require_http_methods(['GET', 'POST'])
+@validate_staff_status(direct_login_page)
+def create_event_view(request):
+    if request.method == 'GET':
+        context = {
+            'form': EventForm
+        }
+        return render(request, 'admin_app/create_event_view.html', context=context)
+    if request.method == 'POST':
+        event = EventForm(request.POST)
+        event.save()
+        return HttpResponseRedirect(reverse('home'))
 
 def photo_upload(request, event_id):
     if request.method == 'POST':
@@ -142,29 +160,29 @@ def photo_upload(request, event_id):
             logger.error(photo_form.errors)
             return HttpResponse('error')
 
-def create_event_view(request):
-    photo_formset = modelformset_factory(Photo, form=PhotoForm, extra=3)
-    if request.method=='POST':
-        event_form = EventForm(request.POST)
-        formset = photo_formset(request.POST, request.FILES, queryset=Photo.objects.none())
-
-        if event_form.is_valid() and formset.is_valid():
-            event = event_form.save()
-
-            for form in formset.cleaned_data:
-                if form:
-                    image = form['image']
-                    photo = Photo(image=image, event=event)
-                    photo.save()
-            return HttpResponseRedirect(reverse('home'))
-    else:
-        event_form = EventForm()
-        formset = photo_formset(queryset=Photo.objects.none())
-        context = {
-            'event_form': event_form,
-            'photo_form': formset
-        }
-        return render(request, 'admin_app/create_event_view.html', context=context)
+# def create_event_view(request):
+#     photo_formset = modelformset_factory(Photo, form=PhotoForm, extra=3)
+#     if request.method=='POST':
+#         event_form = EventForm(request.POST)
+#         formset = photo_formset(request.POST, request.FILES, queryset=Photo.objects.none())
+#
+#         if event_form.is_valid() and formset.is_valid():
+#             event = event_form.save()
+#
+#             for form in formset.cleaned_data:
+#                 if form:
+#                     image = form['image']
+#                     photo = Photo(image=image, event=event)
+#                     photo.save()
+#             return HttpResponseRedirect(reverse('home'))
+#     else:
+#         event_form = EventForm()
+#         formset = photo_formset(queryset=Photo.objects.none())
+#         context = {
+#             'event_form': event_form,
+#             'photo_form': formset
+#         }
+#         return render(request, 'admin_app/create_event_view.html', context=context)
 
 
 
