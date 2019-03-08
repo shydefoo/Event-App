10a93071ebf6922d95728a19b0dfe690230c1ec7
shydefoo <shidefoo@gmail.com>
2019-03-08 17:30:55 +0800

    in the midst of deploying...

--- local_setup/docker_db_start.sh
+++ local_setup/docker_db_start.sh
@@ -4,5 +4,5 @@ docker run --name app_db \
 -e ALLOW_EMPTY_PASSWORD=yes \
 -e MYSQL_DATABASE=event_app_database \
 -p 3306:3306 \
--d \
-bitnami/mysql
+-v local_db_vol:/bitnami/mysql/data \
+-d bitnami/mysql
